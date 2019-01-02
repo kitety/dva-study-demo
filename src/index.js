b396f3e5f044f33148e91bae391a164a8afda699
kitety <aizaizuori@foxmail.com>
2019-01-02 22:30:35 +0800

    初始化项目

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import dva from 'dva';
+import './index.css';
+import RouterConfig from './router'
+
+// 1. Initialize
+const app = dva();
+
+// 2. Plugins
+// app.use({});
+
+// 3. Model
+// app.model(require('./models/example').default);
+
+// 可以先打印
+// console.log(require('./router'))
+// console.log(RouterConfig)
+// 4. Router
+// app.router(require('./router').default);
+app.router(RouterConfig);
+
+// 5. Start
+app.start('#root');
